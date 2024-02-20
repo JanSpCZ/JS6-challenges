c2e95f9a12d8ceac5d1df6df12275a8becb3e4f2
JanSpCZ <jan.spicka147@seznam.cz>
2024-02-20 17:54:14 +0100

    Výpis chybové hlášky při špatném zadání jména a příjmení

--- challenge.js
+++ challenge.js
@@ -8,6 +8,7 @@ document.body.appendChild(container)
 const form1 = document.createElement("form")
 form1.textContent = "Jméno:"
 form1.classList.add("form")
+
 const input1 = document.createElement("input")
 input1.classList.add("input")
 container.appendChild(form1)
@@ -40,9 +41,50 @@ const comment = document.createElement("textarea")
 comment.classList.add("comment")
 container.appendChild(comment)
 
+const errorDiv = document.createElement("div")
+errorDiv.classList.add("error-message")
+errorDiv.classList.add("is-hidden")
+// errorDiv.textContent = "test error message"
+container.appendChild(errorDiv)
+
+
 //2) Při bluru každé kontrolky zapište hodnotu do formDate[kontrolka], pokud splňuje
 //všechna předepsaná valRules
 //Pokud je nesplňuje, zobrazte chybovou hlášku
+
+input1.addEventListener("blur", (e) => {
+    if(e.target.value.length <= 0 || e.target.value.length > 50) {
+        errorDiv.textContent = "Špatně vyplněné křestní jméno!"
+        errorDiv.classList.remove("is-hidden")
+        errorDiv.classList.add("is-visible")
+    } else {
+        errorDiv.classList.remove("is-visible")
+        errorDiv.classList.add("is-hidden")
+    }
+})
+input1.addEventListener("keydown", (e) => {
+    if(/^[0-9]$/.test(e.key)) {
+        e.preventDefault()
+    }
+})
+
+
+input2.addEventListener("blur", (e) => {
+    if(e.target.value.length <= 0 || e.target.value.length > 100) {
+        errorDiv.textContent = "Špatně vyplněné příjmení!"
+        errorDiv.classList.remove("is-hidden")
+        errorDiv.classList.add("is-visible")
+    } else {
+        errorDiv.classList.remove("is-visible")
+        errorDiv.classList.add("is-hidden")
+    }
+})
+input2.addEventListener("keydown", (e) => {
+    if(/^[0-9]$/.test(e.key)) {
+        e.preventDefault()
+    }
+})
+
 const formDate = {
     firstname: "",
     lastname: "",
